e5a1b1fc1a1df2ccde8d70a1818e57f7940718da
Fábio Henrique <fabio.pauleli@gmail.com>
2024-05-20 22:12:16 -0300

    First Commit

---
+++ src/App.jsx
@@ -0,0 +1,19 @@
+import { useState } from "react"
+import Background from './Components/Background/Background';
+
+const App = () => {
+  let heroData = [
+    {text1:"Dive into",text2:"what you love"},
+    {text1:"Indulge",text2:"your passions"},
+    {text1:"Give in to",text2:"your passions"},
+  ]
+  const [heroCount, setHeroCount] =useState(2);
+  const [playStatus,setPlayStatus]= useState(false);
+  return (
+    <div>
+      <Background playStatus={playStatus} heroCount={heroCount}/>
+    </div>
+  )
+}
+
+export default App
---
+++ src/Components/Navbar/Navbar.jsx
@@ -0,0 +1,11 @@
+import './Navbar.css'
+
+const Navbar = () => {
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default Navbar

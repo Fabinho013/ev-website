d528c343484834dd896350995847c9852e7c29b0
Fábio Henrique <fabio.pauleli@gmail.com>
2024-06-24 20:11:35 -0300

    Adição da barra de navegação (navbar)

--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -4,11 +4,13 @@ const Navbar = () => {
   return (
     <div className="nav">
       <div className="nav-logo">EV-olution</div>
-      <ul className="nav-menu"></ul> 
+      <ul className="nav-menu">
+        <li>Home</li>
+        <li>EXplore</li>
+      </ul>
       
     </div>
   )
 }
 
 export default Navbar
-
